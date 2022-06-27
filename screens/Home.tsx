42e64e18e6d0dc1ee75a0f5b1705532430d97cad
Olie Chan <>
2022-06-27 15:23:16 +0100

    install react-native-vector-icons

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {useRootNavigation} from '../navigation';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export function Home() {
   const {navigate} = useRootNavigation();
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        style={{borderWidth: 1, borderColor: 'black'}}
+        style={{borderWidth: 1, borderColor: 'black', padding: 12}}
         onPress={() => navigate('CardPerspective')}>
-        <Text>Hello world</Text>
+        <Text
+          style={{
+            fontSize: 16,
+          }}>
+          Card Perspective
+          <Icon name="stepforward" size={30} color="black" />
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -18,5 +25,7 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
